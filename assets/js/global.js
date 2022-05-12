191e294b7ea84dcdb91fa7e4a251473e15f54df1
devs.hashtag <devs.hashtag@gmail.com>
2022-05-12 14:46:46 +0430

    header cart completed navbar hr added

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,17 +1,40 @@
-const navbarToggle = document.querySelector("div.navbar-toggle");
-const navbar = document.querySelector(".header__navbar");
-const screenOverlay = document.querySelector(".screen-overlay");
-const headerHeight = document.querySelector("header").clientHeight;
-const body = document.body;
-// set default height of navbar items in mobile
-document.documentElement.style.setProperty('--navbar-mobile-height', headerHeight.toString() + 'px');
+window.addEventListener("load", function () {
+  const navbarToggle = document.querySelector("div.navbar-toggle");
+  const navbar = document.querySelector(".header__navbar");
+  const screenOverlay = document.querySelectorAll(".screen-overlay");
+  const cartButton = document.querySelector("header .cart-button");
+  const cartContent = document.querySelector("header .cart-content");
+  const body = document.body;
 
-screenOverlay.addEventListener('click', activeNavbar);
-navbarToggle.addEventListener('click', activeNavbar);
+  const headerHeight = document.querySelector("header").clientHeight;
+
+  // set default height of navbar items in mobile
+  document.documentElement.style.setProperty('--navbar-mobile-height', headerHeight.toString() + 'px');
+
+  // screenOverlay events
+  screenOverlay.forEach(item => {
+    item.addEventListener('click', (elm) => {
+      let element = elm.target.parentNode;
+      element.classList.toggle('active');
+
+      if (element.classList.contains('header__navbar')) {
+        navbarToggle.classList.toggle('active');
+        body.classList.toggle('disable');
+      }
+    })
+  });
+
+  // navbar events
+  navbarToggle.addEventListener('click', activeNavbar);
+  // cart events
+  cartButton.addEventListener('click', () => {
+    cartContent.classList.toggle('active');
+  });
 
 
-function activeNavbar() {
-  navbarToggle.classList.toggle('active');
-  navbar.classList.toggle('active');
-  body.classList.toggle('disable');
-}
+  function activeNavbar() {
+    navbarToggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+    body.classList.toggle('disable');
+  }
+});
