5e684ee71fe09fe89742af5987a9c093d7c7b1d4
devs.hashtag <devs.hashtag@gmail.com>
2022-05-29 11:04:25 +0430

    course__cards class changed to course-cards

--- assets/js/pages/courses.js
+++ assets/js/pages/courses.js
@@ -1,7 +1,7 @@
 import { courseCards } from '/assets/js/modules/functions.js';
 
 window.addEventListener("DOMContentLoaded", function () {
-  const courseCardsClass = ".courses__products > .course__cards";
+  const courseCardsClass = ".courses__products > .course-cards";
   const coursesAPI = "/assets/js/data/product-courses.json";
   courseCards(courseCardsClass, coursesAPI);
 });
