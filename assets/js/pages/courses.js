0a729c788585b368c36c57d547de81c390cecb13
devs.hashtag <devs.hashtag@gmail.com>
2022-06-03 19:59:43 +0430

    courses products options added

--- assets/js/pages/courses.js
+++ assets/js/pages/courses.js
@@ -1,7 +1,31 @@
 import { courseCards } from '/assets/js/modules/functions.js';
 
 window.addEventListener("DOMContentLoaded", function () {
+  // courses cards
   const courseCardsClass = ".courses__products > .course-cards";
   const coursesAPI = "/assets/js/data/product-courses.json";
   courseCards(courseCardsClass, coursesAPI);
+
+  // options order
+  const orderSelected = document.querySelector(".options__order > .order__selected");
+  orderSelected.addEventListener('click', () => {
+    orderSelected.parentNode.classList.toggle('open');
+  });
+
+  // options view
+  const viewGrid = document.querySelector(".options__view > .view-grid");
+  const viewList = document.querySelector(".options__view > .view-list");
+  const productsCards = document.querySelector(courseCardsClass);
+  // view grid
+  viewGrid.addEventListener('click', () => {
+    viewGrid.classList.add('active');
+    viewList.classList.remove('active');
+    productsCards.classList.remove('view-list');
+  });
+  // view list
+  viewList.addEventListener('click', () => {
+    viewList.classList.add('active');
+    viewGrid.classList.remove('active');
+    productsCards.classList.add('view-list');
+  });
 });
