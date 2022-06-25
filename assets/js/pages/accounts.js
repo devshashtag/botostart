0319124dbf6e52ab3ae7c53d02baea72725162dd
devs.hashtag <devs.hashtag@gmail.com>
2022-06-25 17:08:04 +0430

    accounts main added

---
+++ assets/js/pages/accounts.js
@@ -0,0 +1,16 @@
+
+window.addEventListener("DOMContentLoaded", function () {
+  const loginModal = document.querySelector('.account__modal .account__login');
+  const registerModal = document.querySelector('.account__modal .account__register');
+  const showLoginBtn = document.getElementById('showLogin');
+  const showRegisterBtn = document.getElementById('showRegister');
+
+  showRegisterBtn.addEventListener('click', toggleModals);
+  showLoginBtn.addEventListener('click', toggleModals);
+
+  function toggleModals() {
+    registerModal.classList.toggle('active');
+    loginModal.classList.toggle('active');
+  }
+
+});
