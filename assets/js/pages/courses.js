81f13b3a3496788c7742aeb527edd58910b723ad
devs.hashtag <devs.hashtag@gmail.com>
2022-07-12 08:39:29 +0430

    courses options-view fixed

--- assets/js/pages/courses.js
+++ assets/js/pages/courses.js
@@ -3,32 +3,32 @@ import { displayTemplate } from "/assets/js/modules/template.js";
 window.addEventListener("DOMContentLoaded", function () {
   // courses cards
   const courseCardsElm = document.querySelector(".courses__products > .course-cards");
-  const coursesAPI = "/assets/js/data/product-courses.json";
-  displayTemplate(coursesAPI, courseCardsElm, 'course');
-
   // options order
   const orderSelected = document.querySelector(".options__order > .order__selected");
-
-  orderSelected.addEventListener('click', () => {
-    orderSelected.parentNode.classList.toggle('open');
-  });
-
   // options view
   const viewGrid = document.querySelector(".options__view > .view-grid");
   const viewList = document.querySelector(".options__view > .view-list");
-  const productsCards = document.querySelector(courseCardsClass);
+
+  // display courses
+  const coursesAPI = "/assets/js/data/product-courses.json";
+  displayTemplate(coursesAPI, courseCardsElm, 'course');
+
+  // order select dropdown
+  orderSelected.addEventListener('click', () => {
+    orderSelected.parentNode.classList.toggle('open');
+  });
 
   // view grid
   viewGrid.addEventListener('click', () => {
     viewGrid.classList.add('active');
     viewList.classList.remove('active');
-    productsCards.classList.remove('view-list');
+    courseCardsElm.classList.remove('view-list');
   });
 
   // view list
   viewList.addEventListener('click', () => {
     viewList.classList.add('active');
     viewGrid.classList.remove('active');
-    productsCards.classList.add('view-list');
+    courseCardsElm.classList.add('view-list');
   });
 });
