5f0d3ad12a8fb669de6e7f7909d205b10c95ba2c
devs.hashtag <devs.hashtag@gmail.com>
2022-07-14 19:20:06 +0430

    article post added

---
+++ assets/js/pages/article.js
@@ -0,0 +1,57 @@
+// functions
+// clear extra indent from code
+function clearIndent(code) {
+  let codeLines = code.split('\n').filter(item => item.trim());
+  let indentLevel = codeLines[0].match(/^ */)[0].length;
+
+  return codeLines.map(line => {
+    if (!line.slice(0, indentLevel).trim())
+      return line.slice(indentLevel);
+    else
+      return line.replace(/^ */, '');
+  }).join('\n');
+
+}
+
+// remove autofocus attr from elements
+function preventAutofocus(code) {
+  return code.replaceAll(' autofocus=""', '');
+}
+
+
+// events
+window.addEventListener("DOMContentLoaded", function () {
+  let codeExamples = document.querySelectorAll('article.article__post .code-example');
+
+  // display examples, outputs
+  for (const code of codeExamples) {
+    // skip empty code blocks
+    if (!code.innerHTML.trim()) continue;
+
+    let htmlCode = clearIndent(code.innerHTML);
+    let htmlSafeCode = preventAutofocus(htmlCode);
+
+    let errorMsg = '';
+
+    if (htmlCode.search('autofocus') != -1) {
+      errorMsg = '<p><b>نکته: </b>در این مقاله از autofocus جلو گیری میشود. برای دیدن نتیجه کد را سیستم خودتان امتحان کنید</p>';
+    }
+
+    let htmlOutput = `
+  <!-- code output -->
+  <p>خروجی :</p>
+
+  ${errorMsg}
+
+  <code class="code-output">${htmlSafeCode}</code>`;
+
+    // clear current html
+    code.innerHTML = '';
+    // convert code to html entities
+    code.insertAdjacentText('beforeend', htmlCode);
+    // display output after code example
+    code.insertAdjacentHTML('afterend', htmlOutput);
+  }
+
+  document.body.scrollIntoView();
+});
